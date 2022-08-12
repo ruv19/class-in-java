74e58b5527079683bb6f7b7aeb1d554b8b2f0bfd
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-12 10:57:50 +0300

    Update insan.java

--- insan.java
+++ insan.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication4;
 
 public class insan {
@@ -12,33 +7,22 @@ public class insan {
     private int yas;
     private String cevap;
     
-    
-    public String getIsim()
-    {
+    public String getIsim() {
         return isim;
     }
-    public void setIsim(String isim)
-    {
+    public void setIsim(String isim) {
         this.isim = isim;
     }
-    public String getSoyad()
-    {
+    public String getSoyad() {
         return soyad;
     }
-    public void setSoyad(String soyad)
-    {
+    public void setSoyad(String soyad) {
         this.soyad = soyad;
     }
-    
-    public int getYas()
-    {
+    public int getYas() {
         return yas;
     }
-    public void setYas(int yas)
-    {
+    public void setYas(int yas) {
         this.yas = yas;
     }
-    
-   
-    
-    }
+}
