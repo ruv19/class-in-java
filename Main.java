a252fdc2061c68f1a9f9548498c02da8e502f853
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-12 10:58:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
-
 package javaapplication4;
+
 import java.util.Scanner;
 
-public class Main{
-    public static void main (String []args){
+public class Main {
+    public static void main (String []args) {
     
     Scanner scan = new Scanner(System.in);
-    
-    
+
     insan i1 = new insan();
     insan i2= new insan();
-    bisiklet x1=new bisiklet();
+    bisiklet x1 = new bisiklet();
     i1.setIsim("Ahmet");
     i1.setSoyad("Kara");
     i1.setYas(23);
@@ -19,17 +18,8 @@ public class Main{
     i2.setSoyad("Yılmaz");
     i2.setYas(45);
     
-    
     System.out.println("Merhaba "+i1.getIsim()+" "+i1.getSoyad()+" yaşınız "+i1.getYas());
-    
     System.out.println("Merhaba "+i2.getIsim()+" "+i2.getSoyad()+" yaşınız "+i2.getYas());
-    
-     x1.bin(i1);
-    
-    
-    
+    x1.bin(i1);
     }
-    }
-
-    
-
+}
